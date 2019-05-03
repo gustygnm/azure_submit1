b316afb79c31f3bda8b33845813960425dc43e22
Gusti Ngurah <gusti.ngurah.mertayasa@gmail.com>
2019-05-03 09:34:04 -0700

    first commit

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
             $name = $_POST['name'];
             $email = $_POST['email'];
             $job = $_POST['job'];
-            $date = date("Y-m-d");
+            $date = date("yyyy-dd-mm");
             // Insert data
             $sql_insert = "INSERT INTO Registration (name, email, job, date) 
                         VALUES (?,?,?,?)";
